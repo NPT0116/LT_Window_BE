7e5f81b47868b264ad5c14572cb2cf4c5a958571
NPT0116 <thanh1612004@gmail.com>
2025-03-13 14:48:36 +0700

    LTW-29 finish api for getALlVariantWithFullQuery

--- src/Repositories/VariantRepository.cs
+++ src/Repositories/VariantRepository.cs
@@ -4,6 +4,8 @@ using src.Dto.Variant;
 using src.Exceptions.Variant;
 using src.Interfaces;
 using src.Models;
+using src.Query;
+using src.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -79,6 +81,97 @@ namespace src.Repositories
                 SellingPrice = variant.SellingPrice,
                 StockQuantity = variant.StockQuantity
             };
+        }
+
+         public async Task<PagedResponse<List<VariantDto>>> GetAllVariantsAsync(VariantQueryParameters parameters)
+        {
+            var query = _context.Variants
+                .Include(v => v.Color)
+                .Include(v => v.Item)
+                    .ThenInclude(i => i.Manufacturer)
+                .AsQueryable();
+
+if (!string.IsNullOrEmpty(parameters.Search))
+{
+    var searchLower = parameters.Search.ToLower();
+    query = query.Where(v => v.Item.ItemName.ToLower().StartsWith(searchLower));
+}
+
+
+            if (!string.IsNullOrEmpty(parameters.StorageFilter))
+            {
+                query = query.Where(v => v.Storage == parameters.StorageFilter);
+            }
+
+            if (!string.IsNullOrEmpty(parameters.ManufacturerFilter))
+            {
+                query = query.Where(v => v.Item.Manufacturer.ManufacturerName.Contains(parameters.ManufacturerFilter));
+            }
+
+            if (!string.IsNullOrEmpty(parameters.SortBy))
+            {
+                switch (parameters.SortBy.ToLower())
+                {
+                    case "costprice":
+                        query = parameters.SortDirection.ToLower() == "desc"
+                            ? query.OrderByDescending(v => v.CostPrice)
+                            : query.OrderBy(v => v.CostPrice);
+                        break;
+                    case "sellingprice":
+                        query = parameters.SortDirection.ToLower() == "desc"
+                            ? query.OrderByDescending(v => v.SellingPrice)
+                            : query.OrderBy(v => v.SellingPrice);
+                        break;
+                    case "stockquantity":
+                        query = parameters.SortDirection.ToLower() == "desc"
+                            ? query.OrderByDescending(v => v.StockQuantity)
+                            : query.OrderBy(v => v.StockQuantity);
+                        break;
+                    case "storage":
+                        query = SortingUtils.SortByStorage(query, parameters.SortDirection);
+                        break;
+                    default:
+                        query = query.OrderBy(v => v.Storage);
+                        break;
+                }
+            }
+            else
+            {
+                query = query.OrderBy(v => v.Storage);
+            }
+
+            int totalRecords = await query.CountAsync();
+            int totalPages = (int)Math.Ceiling(totalRecords / (double)parameters.PageSize);
+
+            var variants = await query
+                .Skip((parameters.PageNumber - 1) * parameters.PageSize)
+                .Take(parameters.PageSize)
+                .ToListAsync();
+
+            var variantDtos = variants.Select(v => new VariantDto
+            {
+                VariantID = v.VariantID,
+                ItemID = v.ItemID,
+                ColorID = v.ColorID,
+                Storage = v.Storage,
+                CostPrice = v.CostPrice,
+                SellingPrice = v.SellingPrice,
+                StockQuantity = v.StockQuantity,
+            }).ToList();
+
+            Uri CreatePageUri(int page) => new Uri($"http://localhost:5142/api/Variant/GetAll?PageNumber={page}&PageSize={parameters.PageSize}");
+
+            var pagedResponse = new PagedResponse<List<VariantDto>>(variantDtos, parameters.PageNumber, parameters.PageSize)
+            {
+                TotalRecords = totalRecords,
+                TotalPages = totalPages,
+                FirstPage = CreatePageUri(1),
+                LastPage = CreatePageUri(totalPages),
+                NextPage = parameters.PageNumber < totalPages ? CreatePageUri(parameters.PageNumber + 1) : null,
+                PreviousPage = parameters.PageNumber > 1 ? CreatePageUri(parameters.PageNumber - 1) : null
+            };
+
+            return pagedResponse;
         }
     }
 }
